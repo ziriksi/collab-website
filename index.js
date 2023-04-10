0776208e2f16d02a2960e9f80c10d038e96cc870
6w6 <10294104-6w6@users.noreply.replit.com>
2023-04-10 15:03:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const db = new (require('@replit/database'))();
+
+app.use(express.static('public'));
+
+app.get('/', res => {
+  res.sendFile('index.html');
+});
+
+server.listen(3000);
