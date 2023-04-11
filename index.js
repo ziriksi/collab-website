7d63baab6d5b9cc6bfa0acbaa842adef76f51a9a
ziriksi <78670605+ziriksi@users.noreply.github.com>
2023-04-11 15:28:05 +0000

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express');  
 const app = express();
 const server = require('http').Server(app);
 const db = new (require('@replit/database'))();
